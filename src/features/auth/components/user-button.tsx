7afa4b4de5036643c4cbaf81700fe2337b63d9b8
ionztorm <leon.codes@icloud.com>
2024-10-11 12:33:59 +0100

    fix logout redirect

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { UserAvatar } from "@/components/user-avatar";
 import { useAuthActions } from "@convex-dev/auth/react";
@@ -12,30 +12,33 @@ import { Loader, LogOut } from "lucide-react";
 import { useCurrentUser } from "../api/use-current-user";
 
 export const UserButton = () => {
-	const { signOut } = useAuthActions();
-	const { data, isLoading } = useCurrentUser();
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
 
-	if (isLoading)
-		return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  if (isLoading)
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
 
-	if (!data) return null;
+  if (!data) return null;
 
-	const { name, image } = data;
+  const { name, image } = data;
 
-	return (
-		<DropdownMenu modal={false}>
-			<DropdownMenuTrigger className="outline-none relative">
-				<UserAvatar name={name} image={image} size="lg" />
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align="center" side="right" className="w-60">
-				<DropdownMenuItem
-					onClick={async () => await signOut()}
-					className="h-10 cursor-pointer"
-				>
-					<LogOut className="size-4 mr-2" />
-					Log Out
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
-	);
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <UserAvatar name={name} image={image} size="lg" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="center" side="right" className="w-60">
+        <DropdownMenuItem
+          onClick={async () => {
+            await signOut();
+            window.location.reload();
+          }}
+          className="h-10 cursor-pointer"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
